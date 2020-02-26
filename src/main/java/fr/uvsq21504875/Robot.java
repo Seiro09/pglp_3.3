65fcd2fe26a65630cf9c9d8f20ff8ac0734ea06f
Elarif Hamidou <elarif.hamidou@ens.uvsq.fr>
2020-02-26 16:13:58 +0100

    Ajout classe Robot et RobotStatique avec redéfinition de l'héritage.

---
+++ src/main/java/fr/uvsq21504875/Robot.java
@@ -0,0 +1,11 @@
+package fr.uvsq21504875;
+
+public class Robot extends RobotStatique {
+  private Position position;
+  public Robot(){
+    super();
+    //Position.
+  }
+
+  public void avance(){//Faire avancer le robot}
+}
---
+++ src/main/java/fr/uvsq21504875/RobotStatique.java
@@ -0,0 +1,8 @@
+package fr.uvsq21504875;
+
+public class RobotStatique{
+  protected Direction direction;
+
+  public RobotStatique(){}
+  public void tourne(){}
+}
